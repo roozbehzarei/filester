3b2e3ba8feaad9a1630ca3901267baa4bfa18470
Roozbeh Zarei <roozbehzarei@proton.me>
2023-07-27 11:34:02 +0330

    Implement Firebase Analytics, Crashlytics and Performance Monitoring

--- build.gradle.kts
+++ build.gradle.kts
@@ -11,4 +11,7 @@ plugins {
     id("com.android.library") version "8.1.0" apply false
     id("org.jetbrains.kotlin.android") version "1.9.0" apply false
     id("com.google.devtools.ksp") version "1.9.0-1.0.12" apply false
+    id("com.google.gms.google-services") version "4.3.15" apply false
+    id("com.google.firebase.crashlytics") version "2.9.7" apply false
+    id("com.google.firebase.firebase-perf") version "1.4.2" apply false
 }
