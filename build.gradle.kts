8dbc485b46b56f5f628b2c298b6ac2dd27343a9f
Roozbeh Zarei <roozbehzarei@proton.me>
2024-12-05 03:37:50 +0330

    Update SDK and dependencies

--- build.gradle.kts
+++ build.gradle.kts
@@ -7,8 +7,7 @@ buildscript {
 }
 
 plugins {
-    id("com.android.application") version "8.1.0" apply false
-    id("com.android.library") version "8.1.0" apply false
-    id("org.jetbrains.kotlin.android") version "1.9.0" apply false
-    id("com.google.devtools.ksp") version "1.9.0-1.0.12" apply false
+    id("com.android.application") version "8.7.3" apply false
+    kotlin("jvm") version "2.1.0"
+    id("com.google.devtools.ksp") version "2.1.0-1.0.29" apply false
 }
