d8384e9f37e6c42d6abe95c62fb99fe8fd696cf7
Roozbeh Zarei <roozbehzarei@proton.me>
2025-04-18 13:59:43 +0330

    Update Oshi domain address and resolve timeout issue

--- app/src/main/java/com/roozbehzarei/filester/data/network/oshi/OshiApiService.kt
+++ app/src/main/java/com/roozbehzarei/filester/data/network/oshi/OshiApiService.kt
@@ -1,21 +1,25 @@
 package com.roozbehzarei.filester.data.network.oshi
 
 import okhttp3.MultipartBody
+import okhttp3.OkHttpClient
 import retrofit2.Response
 import retrofit2.Retrofit
 import retrofit2.converter.scalars.ScalarsConverterFactory
 import retrofit2.http.Multipart
 import retrofit2.http.POST
 import retrofit2.http.Part
+import java.util.concurrent.TimeUnit
 
-private const val OSHI_URL = "https://oshi.at"
+private const val OSHI_URL = "https://oshi.ec"
+
+private val okHttpClient =
+    OkHttpClient.Builder().readTimeout(2, TimeUnit.HOURS).writeTimeout(2, TimeUnit.HOURS).build()
 
 /**
- * The Retrofit object with the Scalars converter.
+ * The Retrofit object with the Scalars converter and [okHttpClient]
  */
-private val retrofit: Retrofit =
-    Retrofit.Builder().baseUrl(OSHI_URL).addConverterFactory(ScalarsConverterFactory.create())
-        .build()
+private val retrofit: Retrofit = Retrofit.Builder().client(okHttpClient).baseUrl(OSHI_URL)
+    .addConverterFactory(ScalarsConverterFactory.create()).build()
 
 /**
  * A public interface that exposes the [sendFile] method
