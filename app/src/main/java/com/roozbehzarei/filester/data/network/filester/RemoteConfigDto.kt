79d64e6d6a92a2b90b90258a736a37ad2ee31448
roozbehzarei <roozbehzarei@proton.me>
2025-09-29 17:55:49 +0330

    Adapt to response json changes

--- app/src/main/java/com/roozbehzarei/filester/data/network/filester/RemoteConfigDto.kt
+++ app/src/main/java/com/roozbehzarei/filester/data/network/filester/RemoteConfigDto.kt
@@ -1,9 +1,11 @@
 package com.roozbehzarei.filester.data.network.filester
 
+import kotlinx.serialization.SerialName
 import kotlinx.serialization.Serializable
 
 @Serializable
 data class RemoteConfigDto(
-    val latestVersionCode: Int,
+    @SerialName("version_code_latest")
+    val latestVersionCode: Int, @SerialName("version_code_min")
     val minVersionCode: Int
 )
