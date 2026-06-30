03ba518f30368432a3e80b25ecc398523a29cf68
Roozbeh Zarei <r.zarei@oticamp.ir>
2026-06-30 11:11:50 +0330

    Fix: Firebase's performance stub missing

--- app/src/fdroid/kotlin/com/roozbehzarei/filester/service/FirebaseServiceImpl.kt
+++ app/src/fdroid/kotlin/com/roozbehzarei/filester/service/FirebaseServiceImpl.kt
@@ -7,6 +7,8 @@ class FirebaseServiceImpl : FirebaseService {
 
     override fun setAnalyticsCollectionEnabled(isEnabled: Boolean) {}
 
+    override fun setPerformanceMonitoringEnabled(isEnabled: Boolean) {}
+
     override fun setCrashlyticsCollectionEnabled(isEnabled: Boolean) {}
 
     override fun logUploadSuccess() {}
