0430da984a6c318cecaa7869672b092d9ab86407
Roozbeh Zarei <roozbehzarei@proton.me>
2026-07-03 20:16:24 +0330

    Cleanup Acra and Crashlytics leftovers

--- app/src/global/kotlin/com/roozbehzarei/filester/service/FirebaseServiceImpl.kt
+++ app/src/global/kotlin/com/roozbehzarei/filester/service/FirebaseServiceImpl.kt
@@ -2,27 +2,13 @@ package com.roozbehzarei.filester.service
 
 import android.content.Context
 import com.google.firebase.analytics.FirebaseAnalytics
-import com.google.firebase.crashlytics.FirebaseCrashlytics
-import com.google.firebase.perf.FirebasePerformance
 import com.roozbehzarei.filester.domain.service.FirebaseService
 
 class FirebaseServiceImpl(context: Context) : FirebaseService {
-
-    private val firebaseCrashlytics = FirebaseCrashlytics.getInstance()
     private val firebaseAnalytics = FirebaseAnalytics.getInstance(context)
-
-    private val firebasePerformance = FirebasePerformance.getInstance()
 
     override fun setAnalyticsCollectionEnabled(isEnabled: Boolean) {
         firebaseAnalytics.setAnalyticsCollectionEnabled(isEnabled)
-    }
-
-    override fun setPerformanceMonitoringEnabled(isEnabled: Boolean) {
-        firebasePerformance.isPerformanceCollectionEnabled = isEnabled
-    }
-
-    override fun setCrashlyticsCollectionEnabled(isEnabled: Boolean) {
-        firebaseCrashlytics.isCrashlyticsCollectionEnabled = isEnabled
     }
 
     override fun logUploadSuccess() {
