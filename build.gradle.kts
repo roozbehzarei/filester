8907342a4408fb411a031c98eb5897efa9adad3d
Roozbeh Zarei <roozbehzarei@proton.me>
2026-07-25 02:54:22 +0330

    Upgrade dependencies and remove redundant Crashlytics dependency

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,6 +1,5 @@
 plugins {
     alias(libs.plugins.android.application) apply false
-    alias(libs.plugins.firebase.crashlytics) apply false
     alias(libs.plugins.koin.compiler) apply false
     alias(libs.plugins.kotlin.compose) apply false
     alias(libs.plugins.kotlin.dokka) apply false
